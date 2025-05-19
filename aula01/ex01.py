ca8523d12e7797af326afecc84eaefe29c4f69f0
Symon Barreto <symon.barreto@fpf.br>
2025-05-19 11:54:16 -0400

    atividade01: contador de palavras e caracteres

--- aula01/ex01.py
+++ aula01/ex01.py
@@ -1,3 +1,10 @@
-lista = [x for x in range(20) if x % 2 == 0]
-listaRevertida = list(reversed(lista))
-print(listaRevertida)
+"""Este módulo cria uma lista de números pares de 0 a 19 e imprime a lista invertida."""
+
+def main():
+    """Gera uma lista de números pares de 0 a 19, a inverte e imprime."""
+    lista = [x for x in range(20) if x % 2 == 0]
+    lista_revertida = list(reversed(lista))
+    print(lista_revertida)
+
+if __name__ == "__main__":
+    main()
--- trabalho01/analisador_texto.py
+++ trabalho01/analisador_texto.py
@@ -11,8 +11,25 @@ def possui_pontuacao(texto):
     return texto in string.punctuation
 
 
-def limpar_pontuacao(array):
-    return [palavra.rstrip(string.punctuation) for palavra in array]
+def limpar_pontuacao(palavras):
+    palavras_sem_pontuacao = []
+    for palavra in palavras:
+        palavras_sem_pontuacao.append(palavra.strip(string.punctuation))
+    return palavras_sem_pontuacao
+
+
+def contar_palavras(texto):
+    palavras = texto.split(" ")
+    palavras_sem_pontuacao = limpar_pontuacao(palavras)
+    return len(palavras_sem_pontuacao)
+
+
+def contar_caracteres(texto, opcao):
+    if opcao.lower() == "com espaço":
+        return len(texto)
+    elif opcao.lower() == "sem espaço":
+        return len(texto.replace(" ", ""))
+    return 0
 
 
 def texto_valido(texto):
@@ -22,12 +39,12 @@ def texto_valido(texto):
         if not possui_pontuacao(palavra):
             palavras_validas.append(palavra)
     if len(palavras_validas) < 10:
-        print("Texto muito curto")
-        return False
-    elif possui_numeros(texto):
-        print("Texto contém dados mistos")
-        return False
-    return True
+        print("Texto muito curto\n")
+
+    if possui_numeros(texto):
+        print("Texto contém dados mistos\n")
+
+    return None
 
 
 def capitalizar(texto):
@@ -69,14 +86,18 @@ def transformar_retornar_texto(array):
     print(f"Palavras Capitalizadas:\n{texto_capitalizadas}\n")
 
 
-def estatisticas(palavras):
-    pass
+def estatisticas(texto):
+    print(f"Número total de palavras: {contar_palavras(texto)}\n")
+    print(f"Número total de caracteres (com espaço): {contar_caracteres(texto,  "com espaço")}\n")
+    print(f"Número total de caracteres (sem espaço): {contar_caracteres(texto,  "sem espaço")}\n")
 
 
 def analisar_texto(texto):
     palavras = texto.split(" ")
-    if texto_valido(texto):
-        transformar_retornar_texto(palavras)
+
+    texto_valido(texto)
+    transformar_retornar_texto(palavras)
+    estatisticas(texto)
     return None
 
 
